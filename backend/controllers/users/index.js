936e5320f4c4a4c47a44932b3b697c95894d7d78
VKlapan <vladimir.klapan@gmail.com>
2023-01-08 19:59:46 +0200

    added draft of change user boss endpoint

--- backend/controllers/users/index.js
+++ backend/controllers/users/index.js
@@ -1,9 +1,11 @@
 const getAllUsers = require("./getAllUsers");
 const createUser = require("./createUser");
 const loginUser = require("./loginUser");
+const changeUserBoss = require("./changeUserBoss");
 
 module.exports = {
   getAllUsers,
   createUser,
   loginUser,
+  changeUserBoss,
 };
