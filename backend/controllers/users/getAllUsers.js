ecb46b6e98e7e26d408d3a372fc7cfc66bdfc24a
VKlapan <vladimir.klapan@gmail.com>
2023-01-08 17:39:43 +0200

    refactored recursion for formatting feature

--- backend/controllers/users/getAllUsers.js
+++ backend/controllers/users/getAllUsers.js
@@ -10,18 +10,19 @@ const getAllUsers = async (req, res) => {
       res.status(200).json({ code: 200, user: user });
       break;
     case "boss":
-      let boss_subordinates = await getSubSubordinates([{ id }]);
-      let iterationSubordinates = boss_subordinates;
+      let subordinates = await User.find({ boss: id });
+      let subordinates_objects = createUsersObjects(subordinates);
+
+      let inner_subordinates = subordinates_objects;
 
       do {
-        iterationSubordinates = await getSubSubordinates(iterationSubordinates);
-        boss_subordinates = [...boss_subordinates, ...iterationSubordinates];
-      } while (iterationSubordinates.length > 0);
+        inner_subordinates = await getSubSubordinates(inner_subordinates);
+      } while (inner_subordinates.length > 0);
 
       res.status(200).json({
         code: 200,
         user: user,
-        subordinates: boss_subordinates,
+        subordinates: subordinates_objects,
       });
 
       break;
@@ -34,12 +35,20 @@ const getAllUsers = async (req, res) => {
 
 function getSubSubordinates(subordinates) {
   return Promise.all(
-    subordinates.map(({ id }) => id).map(getOwnSubordinates)
+    subordinates.map(async (userObject) => {
+      return (userObject.subordinates = createUsersObjects(
+        await getOwnSubordinates(userObject.user._id)
+      ));
+    })
   ).then((list) => list.flat(1));
 }
 
 function getOwnSubordinates(id) {
   return User.find({ boss: id });
+}
+
+function createUsersObjects(subordinates) {
+  return subordinates.map((user) => (userObject = { user }));
 }
 
 module.exports = getAllUsers;
